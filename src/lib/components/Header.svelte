f236af3d5439f5c9dac9a647eb316498f295c343
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2023-05-28 11:18:58 +0200

    add header menu with tippy

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,7 +1,12 @@
-<script>
+<script lang="ts">
+	import { tippy } from "$actions";
 	import { browser } from "$app/environment";
-import {Navigation} from "$components";
+	import { page } from "$app/stores";
+    import {Navigation} from "$components";
+	import { ChevronDown, ExternalLink } from "lucide-svelte";
+	import LogoutButton from "./logoutButton.svelte";
 
+    $:user = $page.data.user;
 </script>
 <div class="content">
     <div class="left">
@@ -9,7 +14,47 @@ import {Navigation} from "$components";
         <Navigation desktop={false}/>
         {/if}
     </div>
-    <div class="right">right</div>
+    <div class="right">
+        <div id="profile-button">
+            <button class="profile-button" 
+            use:tippy={{
+                content: document.getElementById('profile-menu') || undefined, 
+                onMount:() => {
+                    const template = document.getElementById('profile-menu');
+                    if (template) {
+                        template.style.display = 'block';
+                    }
+                },
+                trigger: 'click',
+                placement: 'bottom-end',
+                interactive: true,
+                theme: 'menu'
+            }}>
+                {#if user?.images && user.images.length > 0}
+                    <img src={user.images[0].url} alt="" />                
+                {/if}
+                {user?.display_name} <span class="visually-hidden">Profile menu</span>
+                <ChevronDown class="profile-arrow" size={22} />
+            </button>
+        </div>
+        <div id="profile-menu" style="display: none;">
+            <div class="profile-menu-content">
+                <ul>
+                    <li>
+                        <a 
+                            href={user?.external_urls.spotify} 
+                            target="_blank" rel="noopener noreferrer"
+                        >
+                            View on Spotify
+                            <ExternalLink focusable="false" aria-hidden size={20}/>
+                        </a>
+                    </li>
+                    <li><a href="/profile">View Profile</a></li>
+                    <li><LogoutButton /></li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </div>
 
 <style lang="scss">
@@ -19,4 +64,56 @@ import {Navigation} from "$components";
         align-items: center;
         width: 100%;
     }
+    .profile-button {
+		background: none;
+		border: 1px solid var(--border);
+		padding: 5px;
+		border-radius: 25px;
+		display: flex;
+		align-items: center;
+		color: var(--text-color);
+		cursor: pointer;
+		:global(.profile-arrow) {
+			margin-left: 3px;
+		}
+		img {
+			width: 28px;
+			height: 28px;
+			border-radius: 100%;
+			margin-right: 10px;
+		}
+		&:hover {
+			background-color: var(--accent-color);
+		}
+	}
+    .profile-menu-content {
+		padding: 5px 0;
+		ul {
+			padding: 0;
+			margin: 0;
+			list-style: none;
+			li {
+				&:hover {
+					background-image: linear-gradient(rgba(255, 255, 255, 0.07) 0 0);
+				}
+				a :global(svg) {
+					vertical-align: middle;
+					margin-left: 10px;
+				}
+				a,
+				:global(button) {
+					display: inline-block;
+					padding: 10px 15px;
+					background: none;
+					border: none;
+					text-decoration: none;
+					cursor: pointer;
+					color: var(--menu-text-color);
+					width: 100%;
+					text-align: left;
+					font-size: functions.toRem(14);
+				}
+			}
+		}
+	}
 </style>
